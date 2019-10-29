66811a7b87071bdaa05dfb97d91bf96c3e0c169f
AlexMethod <alvalex_@hotmail.com>
2019-10-29 09:07:40 -0600

    layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
     <h1>About</h1>
-    <p>This is my about page</p>
-</body>
-</html>
+    <p>This is my about page</p>    
+@endsection
